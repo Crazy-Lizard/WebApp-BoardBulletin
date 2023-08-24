1bbf184986110636662ce77b093188ae06bfc9cf
Crazy-Lizard <ZhenyaIvanov.7791@gmail.com>
2023-08-24 19:45:53 +0300

    Добавьте файлы проекта.

---
+++ WebApp-BoardBulletin/src/Dashboard/Contracts/Dashboard.Contracts/PostDto.cs
@@ -0,0 +1,17 @@
+﻿using Dashboard.Contracts.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.Contracts
+{
+
+    /// <summary>
+    /// Обьявление. 
+    /// </summary>
+    internal class PostDto : BaseDto
+    {
+    }
+}
