f9903a0ffbf695bf8514a65f9a365a20845ae494
Crazy-Lizard <ZhenyaIvanov.7791@gmail.com>
2023-08-25 20:41:28 +0300

    Revert "Update PostController.cs"
    This reverts commit da0d0eb6d11993a2a039a7919388aa216fb2d620.

--- WebApp-BoardBulletin/src/Dashboard/Hosts/Dashboard.Hosts.Api/Controllers/PostController.cs
+++ WebApp-BoardBulletin/src/Dashboard/Hosts/Dashboard.Hosts.Api/Controllers/PostController.cs
@@ -7,56 +7,6 @@ namespace Dashboard.Hosts.Api.Controllers
     /// </summary>
     public class PostController : ControllerBase
     {
-        /// <summary>
-        /// Получает обьявление по идентификатору.
-        /// </summary>
-        /// <param name="id">Идентификатор обьявления.</param>
-        /// <param name="cancellationToken">Отмена операции.</param>
-        /// <returns>Модель обьявления <see cref="PostDto"/></returns>
-        public async Task<IActionResult> GetByIdAsync(Guid id, CancellationToken cancellationToken)
-        {
-            return Ok();
-        }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        public async Task<IActionResult> GetAllAsync(CancellationToken cancellationToken)
-        {
-            return Ok();
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        public async Task<IActionResult> CreateAsync(CancellationToken cancellationToken)
-        {
-            return Ok();
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        public async Task<IActionResult> UpdateAsync(CancellationToken cancellationToken)
-        {
-            return Ok();
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        public async Task<IActionResult> DeleteAsync(Guid id, CancellationToken cancellationToken)
-        {
-            return Ok();
-        }
     }
 }
