96c472ac1df121062500a4090f72f066463fa8dd
Crazy-Lizard <ZhenyaIvanov.7791@gmail.com>
2023-08-25 20:37:17 +0300

    Revert "Merge branch 'develop' into develop1"
    This reverts commit 7d7c2b35c69468910440b654c043f949dd717a0c, reversing
    changes made to 55aa439766a1e8f2cb46f8710b0b107554c734db.

--- WebApp-BoardBulletin/src/Dashboard/Contracts/Dashboard.Contracts/Base/BaseDto.cs
+++ WebApp-BoardBulletin/src/Dashboard/Contracts/Dashboard.Contracts/Base/BaseDto.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Dashboard.Contracts.Base
 {
+
     /// <summary>
     /// Базовый контракт.
     /// </summary>
--- WebApp-BoardBulletin/src/Dashboard/Contracts/Dashboard.Contracts/PostDto.cs
+++ WebApp-BoardBulletin/src/Dashboard/Contracts/Dashboard.Contracts/PostDto.cs
@@ -1,4 +1,4 @@
-using Dashboard.Contracts.Attachment;
+﻿using Dashboard.Contracts.Attachment;
 using Dashboard.Contracts.Base;
 using System;
 using System.Collections.Generic;
